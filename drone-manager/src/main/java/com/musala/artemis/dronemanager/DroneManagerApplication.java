1b33ec5867c15b74e0a13830371f085387043c1a
batyrk <kineevbatyr@gmail.com>
2023-11-28 03:07:11 +0300

    build: init spring boot application. add docker build files

---
+++ drone-manager/src/main/java/com/musala/artemis/dronemanager/DroneManagerApplication.java
@@ -0,0 +1,11 @@
+package com.musala.artemis.dronemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DroneManagerApplication.class, args);
+    }
+}
--- drone-manager/src/main/java/com/musala/artemis/dronemanager/Main.java
+++
@@ -1,7 +0,0 @@
-package com.musala.artemis.dronemanager;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
